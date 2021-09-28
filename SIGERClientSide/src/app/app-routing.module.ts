a177e2b55adff9733184e7f661e988c352dc2e8c
alexB0999 <67486947+alexB0999@users.noreply.github.com>
2021-09-28 14:28:22 -0300

    Eliminacion de componentes innecesarios y modificaciones para mejorar la adaptabilidad

--- SIGERClientSide/src/app/app-routing.module.ts
+++ SIGERClientSide/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetalleEstadoBoletaComponent } from './components/estadoBoleta/detalle-estado-boleta.component';
-import { EditarEstadoBoletaComponent } from './components/estadoBoleta/editar-estado-boleta.component';
-import { ListaEstadoBoletaComponent } from './components/estadoBoleta/lista-estado-boleta.component';
-import { NuevoEstadoBoletaComponent } from './components/estadoBoleta/nuevo-estado-boleta.component';
 import { ViewMainEstadoBoletaComponent } from './components/estadoBoleta/view-main-estado-boleta.component';
 import { DetalleEstadoLicenciaComponent } from './components/estadoLicencia/detalle-estado-licencia.component';
 import { EditarEstadoLicenciaComponent } from './components/estadoLicencia/editar-estado-licencia.component';
@@ -29,11 +25,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'recoveryPassword', component: RecoveryPasswordComponent},
-  //{path: 'estadoBoleta', component: ListaEstadoBoletaComponent},
   {path: 'estadoBoleta', component: ViewMainEstadoBoletaComponent},
-  {path: 'estadoBoleta/detalle/:id', component: DetalleEstadoBoletaComponent},
-  {path: 'estadoBoleta/nuevo', component: NuevoEstadoBoletaComponent},
-  {path: 'estadoBoleta/editar/:id', component: EditarEstadoBoletaComponent},
   {path: 'estadoLicencia', component: ListaEstadoLicenciaComponent},
   {path: 'estadoLicencia/detalle/:id', component: DetalleEstadoLicenciaComponent},
   {path: 'estadoLicencia/nuevo', component: NuevoEstadoLicenciaComponent},
