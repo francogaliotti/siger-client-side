8e7008a4f6c4181ac983a302522c8871cde0504a
francogaliotti <67112060+francogaliotti@users.noreply.github.com>
2021-10-20 16:41:24 -0300

    Abm zona Inhospita terminado

--- SIGERClientSide/src/app/components/zona-inhospita/zona-inhospita.component.ts
+++ SIGERClientSide/src/app/components/zona-inhospita/zona-inhospita.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ANALYZE_FOR_ENTRY_COMPONENTS, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {Modal} from 'bootstrap';
@@ -6,6 +6,7 @@ import { faEdit, faFileAlt, faGrinTongueSquint, faTrash, faPlusCircle } from '@f
 import * as bootstrap from 'bootstrap';
 import { ZonaInhospita } from 'src/app/models/zona-inhospita';
 import { ZonaInhospitaService } from 'src/app/services/zona-inhospita.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-zona-inhospita',
@@ -23,8 +24,11 @@ export class ZonaInhospitaComponent implements OnInit {
   faTrash = faTrash;
   faPlusCircle = faPlusCircle;
 
+  roles: string[];
+  isAdmin = false;
+
   constructor(private _zona: FormBuilder,private _zonaInhospitaService:ZonaInhospitaService, 
-    private router: Router, private _editzona:FormBuilder) {
+    private router: Router, private _editzona:FormBuilder, private _tokenService: TokenService) {
     this.zonaInhospitaForm = this._zona.group({
       codZona: ['', [Validators.required, Validators.maxLength(10)]],
       denominacionZona: ['', Validators.required],
@@ -39,6 +43,12 @@ export class ZonaInhospitaComponent implements OnInit {
 
   ngOnInit(): void {
     this.cargarZonaInhospita();
+    this.roles = this._tokenService.getAuthorities();
+    this.roles.forEach(rol => {
+      if (rol === 'ROLE_ADMIN') {
+        this.isAdmin = true;
+      }
+    })
   }
 
   cargarZonaInhospita():void{
@@ -78,15 +88,38 @@ export class ZonaInhospitaComponent implements OnInit {
     );
   }
   open(id?:number):void{
-
+    this.testModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+      keyboard: false
+    })
+    this.cargarZonaInhospitaForUpdate(id);
+    this.testModal?.show();
   }
+
+  cargarZonaInhospitaForUpdate(id?:number): void{
+    this._zonaInhospitaService.detail(id).subscribe(
+      data=>{
+        this.newZona=data;
+        console.log(this.newZona);
+        this.editZonaForm = this._editzona.group({
+          id: [this.newZona.id, Validators.required],
+          codZona:[this.newZona.codZona,[Validators.required, Validators.maxLength(10)]],
+          denominacionZona: [this.newZona.denominacionZona, Validators.required],
+          precio: [this.newZona.precio, Validators.required]
+        });
+      },
+      err=>{
+        alert(err);
+      }
+    );
+  }
+
   onUpdate(id?: number): void {
     this.newZona.codZona = this.editZonaForm.get('codZona')?.value;
     this.newZona.denominacionZona = this.editZonaForm.get('denominacionZona')?.value;
     this.newZona.precio = this.editZonaForm.get('precio')?.value;
     this._zonaInhospitaService.update(id, this.newZona).subscribe(
       data => {
-        alert('Estado de Licencia actualizado Satisfactoriamente');
+        alert('Zona Inhóspita actualizada Satisfactoriamente');
         this.cargarZonaInhospita();
         this.testModal?.hide();
       },
