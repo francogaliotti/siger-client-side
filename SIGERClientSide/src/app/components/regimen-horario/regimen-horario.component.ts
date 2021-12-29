e7dcb2f7670a3a12929a5115e0f3825573a57aa3
alexB0999 <alexbahi.99@gmail.com>
2021-12-28 21:10:50 -0300

    Tipo regimen horario compontent implemented

---
+++ SIGERClientSide/src/app/components/regimen-horario/regimen-horario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regimen-horario',
+  templateUrl: './regimen-horario.component.html',
+  styleUrls: ['./regimen-horario.component.css']
+})
+export class RegimenHorarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
