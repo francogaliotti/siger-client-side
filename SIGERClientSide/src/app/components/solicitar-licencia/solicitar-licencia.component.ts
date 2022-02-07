7c5f4f61f44fc4ac0ba41a7953272052a7939f80
francogaliotti <67112060+francogaliotti@users.noreply.github.com>
2022-02-07 15:07:59 -0300

    report licencia fixed

--- SIGERClientSide/src/app/components/solicitar-licencia/solicitar-licencia.component.ts
+++ SIGERClientSide/src/app/components/solicitar-licencia/solicitar-licencia.component.ts
@@ -238,6 +238,7 @@ export class SolicitarLicenciaComponent implements OnInit {
             showCloseButton: false,
             showConfirmButton: false
           })
+          console.log(err);
         }
       );
     }
